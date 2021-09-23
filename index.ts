2582a9c4c29cd063753504578bf498b0368327fb
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-09-23 13:49:29 -0400

    binary conversion
    code that takes in a number and returns the number in binary
    
    Needs defensive code added for potential edge cases

---
+++ index.ts
@@ -0,0 +1,38 @@
+let binaryNumber = []
+placeholder: Number;
+
+function startHere(userNumber) {
+    if(userNumber >= 1) {
+        compute(userNumber)
+    } else {console.log(binaryNumber)}
+}
+
+function compute(startingNumber) {
+    const num = startingNumber/2
+    this.placeholder = convertToBinary(num)
+    startHere(this.placeholder)
+}
+
+function convertToBinary(input) {
+    //this.binaryNumber = [];
+    let workingNum = isDecimal(input);
+
+    if (workingNum) {
+        binaryNumber.push(1)
+        return Math.floor(input)
+    } else {
+       binaryNumber.push(0)
+        return input
+    }
+}
+
+function isDecimal(number) {
+  var result = (number - Math.floor(number)) !== 0; 
+
+  if (result)
+    return true;
+   else
+     return false;
+}
+
+startHere(500)
